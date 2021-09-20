bb7cf7589a225d79eda4ba3fd842e464992c7f33
ShalithAlahakoon <shalitha97068@gmail.com>
2021-09-20 16:58:03 +0530

    Medical Insert Form UI implemented

--- app/src/main/java/com/farmmanagementpro/MainActivity.java
+++ app/src/main/java/com/farmmanagementpro/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
 import android.view.WindowManager;
 
 
@@ -20,8 +20,6 @@ import com.yarolegovich.slidingrootnav.SlidingRootNav;
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
 import java.util.Arrays;
-
-import io.grpc.Context;
 
 public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener{
 
@@ -176,6 +174,19 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
 
         slideRootNav.closeMenu();
         transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void InsertMedicineFragmentChange(View view){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        MedicalIsertFragment fragment = new MedicalIsertFragment();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+    }
+    public void MedicalCabinet(View view){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        MedicalCabinetFragment  fragment = new MedicalCabinetFragment();
+        transaction.replace(R.id.container, fragment);
         transaction.commit();
     }
 }
