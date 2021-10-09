55f479dbdcf0247dad5784dd242fe78f78da47ee
ShalithAlahakoon <shalitha97068@gmail.com>
2021-10-09 13:22:04 +0530

    medicle Update implemented

--- app/src/main/java/com/farmmanagementpro/UpdateMedicalFragment.java
+++ app/src/main/java/com/farmmanagementpro/UpdateMedicalFragment.java
@@ -1,6 +1,5 @@
 package com.farmmanagementpro;
 
-
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
@@ -8,74 +7,75 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
-
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.farmmanagementpro.helper.HelperMethod;
+import com.farmmanagementpro.modals.Animal;
 import com.farmmanagementpro.modals.Medicine;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.sdsmdg.tastytoast.TastyToast;
+import com.squareup.picasso.Picasso;
 
-
-public class MedicalIsertFragment extends Fragment  {
-
+public class UpdateMedicalFragment extends Fragment {
     private static final int GALLERY_CODE = 1;
 
     private ImageView uploadImage;
-//    TextView purchaseDate, name, qty, prescribedBy,PrescribedBy;
     private EditText purchaseDateEt, nameEt, qtyEt, prescribedByEt,SupplierValue;
-    private Button resetBtn,saveBtn;
+    private Button resetBtn,updateBtn;
     private Uri imageUri;
 
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener authStateListener;
-    private FirebaseUser currentUser;
-
-    //fireStore connection
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference medicineCollection = db.collection("medicine");
     private StorageReference storageReference;
+
+    private String name;
+    private String imageUrl;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.medical_insert_fragment, container, false);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            name = bundle.getString("name", ""); // Key, default value
+        }
+        return inflater.inflate(R.layout.fragment_update_medical, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull  View v, @Nullable  Bundle savedInstanceState) {
-        uploadImage = v.findViewById(R.id.uploadImageView);
-        purchaseDateEt = v.findViewById(R.id.editTextDate);
-        nameEt = v.findViewById(R.id.editTextName);
-        qtyEt = v.findViewById(R.id.editTextQty);
-        prescribedByEt =v.findViewById(R.id.editTextPrescribeBy);
-        SupplierValue = v.findViewById(R.id.editTextSupplier);
-        resetBtn = v.findViewById(R.id.resetBtn);
-        saveBtn = v.findViewById(R.id.saveBtn);
+        super.onViewCreated(v, savedInstanceState);
+        uploadImage = v.findViewById(R.id.updateMedicalImage);
+        purchaseDateEt = v.findViewById(R.id.editTextDateUpdateMedical);
+        nameEt = v.findViewById(R.id.editTextNameUpdateMedical);
+        qtyEt = v.findViewById(R.id.editTextQtyUpdateMedical);
+        prescribedByEt =v.findViewById(R.id.editTextPrescribeByUpdateMedical);
+        SupplierValue = v.findViewById(R.id.editTextSupplierUpdateMedical);
+        resetBtn = v.findViewById(R.id.resetBtnUpdateMedical);
+        updateBtn = v.findViewById(R.id.saveBtnUpdateMedical);
 
         storageReference = FirebaseStorage.getInstance().getReference();
 
@@ -97,49 +97,64 @@ public class MedicalIsertFragment extends Fragment  {
                 uploadImage.setImageDrawable(getResources().getDrawable(R.drawable.upload_image));
             }
         });
-        
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String date = purchaseDateEt.getText().toString().trim();
+                String name = nameEt.getText().toString().trim();
+                String qty = qtyEt.getText().toString().trim();
+                String prescribedBy = prescribedByEt.getText().toString().trim();
+                String supplier = SupplierValue.getText().toString().trim();
 
-                if (TextUtils.isEmpty(purchaseDateEt.getText().toString().trim())){
-                    TastyToast.makeText(
-                            getActivity(),
-                            "Please enter purchase date !",
-                            TastyToast.LENGTH_LONG,
-                            TastyToast.ERROR
-                    );
-                }else if (TextUtils.isEmpty(nameEt.getText().toString().trim())){
-                    TastyToast.makeText(
-                            getActivity(),
-                            "Please enter Name !",
-                            TastyToast.LENGTH_LONG,
-                            TastyToast.ERROR
-                    );
-                }else if (TextUtils.isEmpty(qtyEt.getText().toString().trim())){
-                    TastyToast.makeText(
-                            getActivity(),
-                            "Please enter event name !",
-                            TastyToast.LENGTH_LONG,
-                            TastyToast.ERROR
-                    );
-                }else{
-                    String date = purchaseDateEt.getText().toString().trim();
-                    String name = nameEt.getText().toString().trim();
-                    String qty = qtyEt.getText().toString().trim();
-                    String prescribedBy = prescribedByEt.getText().toString().trim();
-                    String Supplier = SupplierValue.getText().toString().trim();
-                    
-                    addMedicineImage(date,name,qty,prescribedBy,Supplier);
-                }
 
+
+                deleteMedicine(date,name,qty,prescribedBy,supplier);
             }
         });
-        
+
+        getMedicine(name);
     }
 
-    private void addMedicineImage(String date, String name, String qty, String prescribedBy, String supplier) {
+    private void getMedicine(String name) {
+        medicineCollection.whereEqualTo("name",name)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for(QueryDocumentSnapshot medicines : queryDocumentSnapshots){
+                            Medicine medicine = medicines.toObject(Medicine.class);
+                            purchaseDateEt.setText(medicine.getDate());
+                            nameEt.setText(medicine.getName());
+                            qtyEt.setText(medicine.getQty());
+                            prescribedByEt.setText(medicine.getPrescribedBy());
+                            SupplierValue.setText(medicine.getSupplier());
 
+                            imageUrl = medicine.getImage();
+
+                            Picasso.get()
+                                    .load(medicine.getImage())
+                                    .placeholder(R.drawable.upload_image)
+                                    .fit()
+                                    .into(uploadImage);
+
+                        }
+                    }
+                });
+    }
+
+    private void deleteMedicine(String date, String name, String qty, String prescribedBy, String supplier) {
+        medicineCollection.document(name)
+                .delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateMedicineImage(date,name,qty,prescribedBy,supplier);
+                    }
+                });
+    }
+
+    private void updateMedicineImage(String date, String name, String qty, String prescribedBy, String supplier) {
         Medicine medicine = new Medicine();
         medicine.setDate(date);
         medicine.setName(name);
@@ -147,7 +162,7 @@ public class MedicalIsertFragment extends Fragment  {
         medicine.setPrescribedBy(prescribedBy);
         medicine.setSupplier(supplier);
 
-        if(imageUri != null){
+        if (imageUri != null){
             StorageReference filePath = storageReference.child("medicine").child(name);
             filePath.putFile(imageUri)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -164,9 +179,7 @@ public class MedicalIsertFragment extends Fragment  {
                                     .addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
-                                            int duration = Toast.LENGTH_SHORT;
-                                            Toast toast = Toast.makeText(getContext(),"Error happened while uploading image.", duration);
-                                            toast.show();
+                                            HelperMethod.createErrorToast(getActivity(),"Error happened while uploading image.");
                                         }
                                     });
                         }
@@ -174,15 +187,14 @@ public class MedicalIsertFragment extends Fragment  {
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            int duration = Toast.LENGTH_SHORT;
-                            Toast toast = Toast.makeText(getContext(),"Error happened while uploading image.", duration);
-                            toast.show();
+                            HelperMethod.createErrorToast(getActivity(),"Error happened while uploading image.");
                         }
                     });
         }else{
-            medicine.setImage("Not Added");
+            medicine.setImage(imageUrl);
             addMedicineDetails(medicine);
         }
+
     }
 
     private void addMedicineDetails(Medicine medicine) {
@@ -190,12 +202,20 @@ public class MedicalIsertFragment extends Fragment  {
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        nameEt.setText("");
                         purchaseDateEt.setText("");
+                        nameEt.setText("");
                         qtyEt.setText("");
                         prescribedByEt.setText("");
                         SupplierValue.setText("");
                         uploadImage.setImageDrawable(getResources().getDrawable(R.drawable.upload_image));
+
+
+                        MedicalCabinetFragment medicalCabinetFragment = new MedicalCabinetFragment();
+
+                        getFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.container, medicalCabinetFragment)
+                                .commit();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -209,6 +229,7 @@ public class MedicalIsertFragment extends Fragment  {
                         );
                     }
                 });
+
     }
 
     private void showDialog() {
@@ -234,17 +255,4 @@ public class MedicalIsertFragment extends Fragment  {
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
         dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == GALLERY_CODE && resultCode == -1){
-            if(data != null){
-                imageUri = data.getData();
-                uploadImage.setImageURI(imageUri);
-                Log.d("Image uri", imageUri.toString());
-            }
-        }
-    }
-
 }
